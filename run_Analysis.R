886cf7cef6f80c6c24cd039838c18decf1dc1422
lsolomyak <lsolomyak@gmail.com>
2017-04-20 11:59:49 +0300

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -15,7 +15,7 @@ test_set <- matrix(test_set, ncol=561)
 #turn the set into a table
 test_set <- tbl_df(test_set)
 #turn all the values into numerics 
-test_set <- tbl_df(sapply(test_set, as.numeric)) #tbl_df(
+test_set <- tbl_df(sapply(test_set, as.numeric)) 
 
 # same thing for the training set 
 train_set <- read.csv(directory[27], header = FALSE)
@@ -53,10 +53,12 @@ subject_test <- read.csv(directory[14], header = FALSE, stringsAsFactors = TRUE)
 train_set_lab <- as.data.frame(train_set_lab)
 test_set_lab <- as.data.frame(test_set_lab)
 
-un_names <- ave(as.character(names(testset)), names(testset), FUN=function(x) if (length(x)>1) paste0(x[1], '(', seq_along(x), ')') else x[1])
-unique_names <- as.data.frame(un_names)
 tset <- cbind(subject_train$V1,train_set_lab, train_set)
 testset <- cbind(subject_test$V1, test_set_lab, test_set)
+un_names <- ave(as.character(names(testset)), names(testset), FUN=function(x) if (length(x)>1) paste0(x[1], '(', seq_along(x), ')') else x[1])
+unique_names <- as.data.frame(un_names)
+
+
 
 names(tset) <-  unique_names$un_names
 names(testset) <-  unique_names$un_names
@@ -68,20 +70,20 @@ testset <- tbl_df(testset)
 # now we add the labels and subject number and combine data sets
 names(tset) <- names(testset)
 fullset <- rbind(tset, testset)
-names(fullset) <- unique_names$un_names
+#names(fullset) <- unique_names$un_names
 #names(fullset) <- c("`subject_train$V1`","train_set_lab", features$`features[, 2]`)
 fullset <- tbl_df(fullset)
 #make sure the subject number is a factor 
-fullset$`subject_train$V1` <- as.factor(fullset$`subject_train$V1`)
+fullset$`subject_test$V1` <- as.factor(fullset$`subject_test$V1`)
 #lets only choose the columns we need
-fullset <- select(fullset,`subject_train$V1`, train_set_lab, grep("mean" , names(fullset)) , grep("std" , names(fullset)))
+fullset <- select(fullset,`subject_test$V1`, test_set_lab, grep("mean" , names(fullset)) , grep("std" , names(fullset)))
 
 #first we group by id then by activity
 #and rename the first two columns- first all the means then all the std 
 
 # now we get the summary variable called data_summary 
-fullset$`subject_train$V1` <- as.factor(fullset$`subject_train$V1`)
-data_summary <- fullset %>% group_by(`subject_train$V1`, train_set_lab) %>% summarise_each(funs(mean), `tBodyAcc-mean()-X`:`fBodyBodyGyroJerkMag-std()`)
-data_summary <- rename(data_summary,subject= `subject_train$V1` , activity = train_set_lab)
+fullset$`subject_test$V1` <- as.factor(fullset$`subject_test$V1`)
+data_summary <- fullset %>% group_by(`subject_test$V1`, test_set_lab) %>% summarise_each(funs(mean), `tBodyAcc-mean()-X`:`fBodyBodyGyroJerkMag-std()`)
+data_summary <- rename(data_summary,subject= `subject_test$V1` , activity = test_set_lab)
 
-write.table(data_summary, "summarized_data.csv", col.names = TRUE )
+write.table(data_summary, "~/Downloads/summarized_data.csv", row.names = FALSE)
